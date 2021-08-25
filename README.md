27873595e4a09b2d0a04a0b790600bd7751ac3f0
adellamaggiora <48822599+adellamaggiora@users.noreply.github.com>
2021-08-25 14:38:03 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,28 @@
+# easy-circular-buffer
+
+### Easy to use and high performance circular buffer
+
+
+
+#### Installation
+
+`npm i easy-circular-buffer`
+
+
+#### Usage
+
+`const { createCircularBuffer } = require('easy-circular-buffer')`
+
+`const cb = createCircularBuffer(length)` 
+
+
+##### Methods
+
+* `cb.insert()` <span style="color: forestgreen"> <*void*> </span> <span> Insert item into circular buffer
+* `cb.peekNewest()` <span style="color: forestgreen"> <*any*> </span> <span> Returns the newest item inserted in the circular buffer
+</span>
+* `cb.peekOldest()` <span style="color: forestgreen"> <*any*> </span> <span> Returns the oldest item inserted in the circular buffer
+</span>
+* `cb.toArray()` <span style="color: forestgreen"> <*Array<*any*>*>  </span> Returns an array sorted from oldest (at index 0) to newest item.
+
+
---
+++ index.js
@@ -0,0 +1,41 @@
+function createCircularBuffer(length) {
+    let newest = -1
+    let oldest = 0
+    let buffer = []
+
+    return {
+        insert: item => {
+            newest = (newest + 1) % length
+            if (buffer.length >= length) {
+                oldest = (oldest + 1) % length   
+            }
+            buffer[newest] = item
+        },
+        peekNewest: () => {
+            return buffer[newest]
+        },
+        peekOldest: () => {
+            return buffer[oldest]
+        },
+        toArray: () => {
+            console.log(buffer)
+ 
+            if (newest > oldest) {
+                return buffer.slice(0, newest + 1)
+            }
+            else {
+                const firstPart = buffer.slice(oldest, length)
+                const secondPart = buffer.slice(0, newest + 1)
+                return [...firstPart, ...secondPart]
+            }
+        }
+    }
+
+}
+
+
+module.exports = { createCircularBuffer }
+
+
+
+
